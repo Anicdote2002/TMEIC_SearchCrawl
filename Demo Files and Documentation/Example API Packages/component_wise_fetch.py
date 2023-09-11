136e90cb7adb978980812af2f0a73ffc67ec2fa4
Aniruddh Chauhan <77602855+Anicdote2002@users.noreply.github.com>
2023-09-11 15:37:16 -0400

    completed basic component fetch algorithm along with text output

--- Demo Files and Documentation/Example API Packages/component_wise_fetch.py
+++ Demo Files and Documentation/Example API Packages/component_wise_fetch.py
@@ -13,3 +13,48 @@ api_key = "9yvSoV8p575uVbPhPxgX9vIqJDbzatyuwMKlTrFV"
 url = f"{base_url}?id&api_key={api_key}"
 response = requests.get(url)
 json_data = response.json() # gets .json of bugs from bugzilla
+
+cranePLC_issue_count = 0
+maxview_issue_count  = 0
+general_issue_count  = 0
+
+
+bug_cranePLC_list = []
+bug_maxview_list  = []
+bug_general_list  = []
+
+
+for bug_comp in json_data["bugs"]:
+    if(bug_comp["component"] == "Crane PLC"):
+        bug_cranePLC_list.append(bug_comp["summary"])
+        cranePLC_issue_count = cranePLC_issue_count + 1
+
+    elif(bug_comp["component"] == "Maxview"):
+        bug_maxview_list.append(bug_comp["summary"])
+        maxview_issue_count = maxview_issue_count + 1
+
+    elif(bug_comp["component"] == "General"):
+        bug_general_list.append(bug_comp["summary"])
+        general_issue_count = general_issue_count + 1
+
+
+with open("output_bug_cranePLC_list.txt", "w") as file1:
+    for item in bug_cranePLC_list:
+        file1.write(item + "\n")
+
+# Output list2 to a text file
+with open("output_bug_maxview_list.txt", "w") as file2:
+    for item in bug_maxview_list:
+        file2.write(item + "\n")
+
+# Output list3 to a text file
+with open("output_bug_general_list.txt", "w") as file3:
+    for item in bug_general_list:
+        file3.write(item + "\n")
+
+    
+
+
+print("Number of Crane PLC Issues: ", cranePLC_issue_count)
+print("Number of Maxview Issues  : ", maxview_issue_count )
+print("Number of General Issues  : ", general_issue_count )
