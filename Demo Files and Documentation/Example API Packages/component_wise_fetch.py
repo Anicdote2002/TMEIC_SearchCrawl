50540656dc908d7386797e83ce66ee1de2172395
Aniruddh Chauhan <77602855+Anicdote2002@users.noreply.github.com>
2023-09-11 16:02:17 -0400

    completed basic component fetch algorithm along with text output

--- Demo Files and Documentation/Example API Packages/component_wise_fetch.py
+++ Demo Files and Documentation/Example API Packages/component_wise_fetch.py
@@ -7,6 +7,7 @@ from PIL import Image
 from collections import defaultdict, Counter
 import os
 import re
+import filter_keywords
 
 base_url = "https://tools.tmeic.com/mh/rest/bug"
 api_key = "9yvSoV8p575uVbPhPxgX9vIqJDbzatyuwMKlTrFV"
@@ -18,11 +19,18 @@ cranePLC_issue_count = 0
 maxview_issue_count  = 0
 general_issue_count  = 0
 
-
 bug_cranePLC_list = []
 bug_maxview_list  = []
 bug_general_list  = []
 
+common_words = ["The"   , "in"   , "on"    , "were"  , "of"    , "not"    ,
+                "to"    , "is"   , "a"     , "as"    , "an"    , "that"   , 
+                "the"   , "and"  , "but"   , "has"   , "have"  , "move"   ,
+                "where" , "was"  , "can"   , "cannot", "be"    , "In"     ,
+                "should", "would", "could" , "with"  , "for"   , "if"     ,
+                "when"  , "what" , "after" , "into"  , "update", "between",
+                "why"   , "color", "scheme", "test"  , "do "   , "from"  
+                "been"  , "issue", "needs" , "need"                        ]
 
 for bug_comp in json_data["bugs"]:
     if(bug_comp["component"] == "Crane PLC"):
@@ -52,9 +60,30 @@ with open("output_bug_general_list.txt", "w") as file3:
     for item in bug_general_list:
         file3.write(item + "\n")
 
-    
 
+# Get Filtetred Data i.e. relevant Keywords
+cranePLC_filtered_data = [filter_keywords.remove_words(summary_cranePLC, common_words) for summary_cranePLC in bug_cranePLC_list]
+maxview_filtered_data  = [filter_keywords.remove_words(summary_Maxview, common_words)  for summary_Maxview  in bug_maxview_list ]
+general_filtered_data  = [filter_keywords.remove_words(summary_General, common_words)  for summary_General  in bug_general_list ]
 
+filter_keywords.keyword_max_counter(cranePLC_filtered_data)
+filter_keywords.keyword_max_counter(maxview_filtered_data)
+filter_keywords.keyword_max_counter(general_filtered_data)
+
+with open("output_keyword_cranePLC_list.txt", "w") as keyword_file1:
+    for item in bug_cranePLC_list:
+        keyword_file1.write(item + "\n")
+
+# Output list2 to a text file
+with open("output_keyword_maxview_list.txt", "w") as keyword_file2:
+    for item in bug_maxview_list:
+        keyword_file2.write(item + "\n")
+
+# Output list3 to a text file
+with open("output_keyword_general_list.txt", "w") as keyword_file3:
+    for item in bug_general_list:
+        keyword_file3.write(item + "\n")
+                            
 print("Number of Crane PLC Issues: ", cranePLC_issue_count)
 print("Number of Maxview Issues  : ", maxview_issue_count )
 print("Number of General Issues  : ", general_issue_count )
--- Demo Files and Documentation/Example API Packages/filter_keywords.py
+++ Demo Files and Documentation/Example API Packages/filter_keywords.py
@@ -41,9 +41,12 @@ filtered_data = [remove_words(summary, common_words) for summary in bug_sum_list
 for j in filtered_data:
     print(j)
 
-all_filtered_data = " ".join(filtered_data)
-cleaned_filtered_data = re.sub(r'[^\w\s]', '', all_filtered_data)
-filtered_data_wrds = cleaned_filtered_data.split()# Tokenize the large string into words (split by space)
-word_counts = Counter(filtered_data_wrds)# Count the frequency of each word
-max_word, max_count = word_counts.most_common(1)[0]# Find the word with the maximum frequency
-print(f"The word with the maximum frequency is '{max_word}' with a frequency of {max_count}.")
+def keyword_max_counter(filtered_data):
+    all_filtered_data = " ".join(filtered_data)
+    cleaned_filtered_data = re.sub(r'[^\w\s]', '', all_filtered_data)
+    filtered_data_wrds = cleaned_filtered_data.split()# Tokenize the large string into words (split by space)
+    word_counts = Counter(filtered_data_wrds)# Count the frequency of each word
+    max_word, max_count = word_counts.most_common(1)[0]# Find the word with the maximum frequency
+    print(f"The word with the maximum frequency is '{max_word}' with a frequency of {max_count}.")
+
+keyword_max_counter(filtered_data)
